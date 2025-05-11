b0f19b3c1eb9edc2b7e4c93c681b8766c8c65ac5
SSMG4 <matthew.gallon0912@gmail.com>
2025-05-11 16:34:14 +0200

    Update CBPSDB.java

--- app/src/main/java/eparon/vhbb_android/Constants/CBPSDB.java
+++ app/src/main/java/eparon/vhbb_android/Constants/CBPSDB.java
@@ -1,4 +1,4 @@
-package eparon.vhbb_android.Constants;
+package ssmga.vhbb_android.Constants;
 
 /**
  * Constants relating to CBPS-DB
@@ -36,7 +36,7 @@ public class CBPSDB {
 
 
     //region URL
-    public static final String CVS_URL_RAW      = "https://raw.githubusercontent.com/KuromeSan/cbps-db/master/cbpsdb.csv";
+    public static final String CVS_URL_RAW      = "https://raw.githubusercontent.com/LiEnby/cbps-db/refs/heads/master/cbpsdb.csv";
     //endregion
 
 }
