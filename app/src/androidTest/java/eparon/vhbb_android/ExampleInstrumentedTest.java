9bdbb546ee7de612397e60e6d7d5a041fc391ef8
SSMG4 <matthew.gallon0912@gmail.com>
2025-05-11 16:39:20 +0200

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/eparon/vhbb_android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/eparon/vhbb_android/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package eparon.vhbb_android;
+package ssmga.vhbb_android;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext () {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("eparon.vhbb_android", appContext.getPackageName());
+        assertEquals("ssmga.vhbb_android", appContext.getPackageName());
     }
 }
