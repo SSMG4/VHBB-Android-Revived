018df768e727c051e20605ce3a035bdd8f233890
SSMG4 <matthew.gallon0912@gmail.com>
2025-05-11 17:13:23 +0200

    Update PluginsFragment.java

--- app/src/main/java/ssmga/vhbb_android/ui/plugins/PluginsFragment.java
+++ app/src/main/java/ssmga/vhbb_android/ui/plugins/PluginsFragment.java
@@ -1,4 +1,4 @@
-package eparon.vhbb_android.ui.plugins;
+package ssmga.vhbb_android.ui.plugins;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -25,8 +25,8 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-import eparon.vhbb_android.Constants.VHBBAndroid;
-import eparon.vhbb_android.R;
+import ssmga.vhbb_android.Constants.VHBBAndroid;
+import ssmga.vhbb_android.R;
 
 public class PluginsFragment extends Fragment {
 
