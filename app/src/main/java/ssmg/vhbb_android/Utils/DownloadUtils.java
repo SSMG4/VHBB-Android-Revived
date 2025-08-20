b00cf92743d9ca6d8b045ba00bc0ae6c7ed619a2
SSMG4 <matthew.gallon0912@gmail.com>
2025-08-20 23:27:07 +0200

    Update DownloadUtils

--- app/src/main/java/ssmg/vhbb_android/Utils/DownloadUtils.java
+++ app/src/main/java/ssmg/vhbb_android/Utils/DownloadUtils.java
@@ -1,4 +1,4 @@
-package ssmga.vhbb_android.Utils;
+package ssmg.vhbb_android.Utils;
 
 import android.Manifest;
 import android.app.Activity;
@@ -15,8 +15,8 @@ import androidx.preference.PreferenceManager;
 
 import java.io.File;
 
-import ssmga.vhbb_android.Constants.VitaDB;
-import ssmga.vhbb_android.R;
+import ssmg.vhbb_android.Constants.VitaDB;
+import ssmg.vhbb_android.R;
 
 public class DownloadUtils {
 
