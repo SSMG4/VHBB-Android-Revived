f3db5d2c9ed627141343858f0942660be0e3daa1
SSMG4 <matthew.gallon0912@gmail.com>
2025-08-20 23:19:14 +0200

    Update App Info

--- app/src/androidTest/java/ssmg/vhbb_android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ssmg/vhbb_android/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package ssmga.vhbb_android;
+package ssmg.vhbb_android;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext () {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ssmga.vhbb_android", appContext.getPackageName());
+        assertEquals("ssmg.vhbb_android", appContext.getPackageName());
     }
 }
